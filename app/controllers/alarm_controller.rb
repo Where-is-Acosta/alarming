15f3e06637bf78998f9fa064708628444f72a562
Driptap <sg.sinan@gmail.com>
2014-03-07 20:18:12 +0100

    Gemfile updated

--- app/controllers/alarm_controller.rb
+++ app/controllers/alarm_controller.rb
@@ -1,2 +1,33 @@
 class AlarmController < ApplicationController
+
+  def new 
+
+  end
+
+  def create
+    @alarm = Alarm.new(new_alarm_params)
+
+  end
+
+  def update
+    if @alarm.update(edit_group_params)
+      redirect_to alarm_path, notice: "Alarm is set"
+    else
+      render "edit"
+    end
+  end
+
+  def show
+
+  end
+
+
+  private
+  
+  def new_alarm_params
+    params.permit(:name, :time, :days, :snooze)
+  end
+  def edit_alarm_params
+    params.permit(:name, :time, :days, :snooze)
+  end
 end
