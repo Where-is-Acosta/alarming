cc7278e59ca6a7a7d2cf801c3114c325c120db18
driptap <sg.sinan@gmail.com>
2014-05-06 00:36:28 +0200

    Added json reconstructer for autoplay

--- app/controllers/concerns/mixcloud_api.rb
+++ app/controllers/concerns/mixcloud_api.rb
@@ -4,6 +4,10 @@ module MixcloudApi
   	def get_the_popular_one
   		list = RestClient.get(MIXCLOUD[:hotlist], {:params => {:limit => 1}})
   		parse = JSON.parse(list)
-  		return "http://api.mixcloud.com#{parse ["data"].first["key"]}embed-html/"
+  		mix = RestClient.get("http://api.mixcloud.com#{parse ["data"].first["key"]}embed-json/")
+  		mix.sub! 'src="//', 'src="http://'
+  		mix.sub! 'autoplay=', 'autoplay=1'
+  		object = JSON.parse(mix)
+  		return object ["html"]
   	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :alarms
   	has_secure_password validations: false
-
-	    validates :password, presence: true, on: :create
-	    validates_length_of :password, minimum: 6, on: :create
-	    validates_confirmation_of :password, on: :create
-
-
-
-
-
+    validates :password, presence: true, on: :create
+    validates_length_of :password, minimum: 6, on: :create
+    validates_confirmation_of :password, on: :create
+    validates :client_address, presence: true
 end
